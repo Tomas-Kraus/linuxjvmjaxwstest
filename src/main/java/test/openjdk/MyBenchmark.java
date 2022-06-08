11d3dc6d2e38a472c1fbc6eba5b5e6dd208d252b
Tomas Kraus <tomas.kraus@oracle.com>
2022-06-08 13:16:22 +0200

    Modified test setup to run inprofiler
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

--- src/main/java/test/openjdk/MyBenchmark.java
+++ src/main/java/test/openjdk/MyBenchmark.java
@@ -30,6 +30,9 @@ public class MyBenchmark {
         }
     }
 
+    @Fork(value = 0, warmups = 0)
+    @Warmup(iterations = 3)
+    @Measurement(iterations = 3)    
     @Benchmark
     public void testMethod(MyState state) throws Exception {
         String result = state.client.foo(state.foo);
